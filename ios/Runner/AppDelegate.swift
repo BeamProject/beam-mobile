12c3af28b7e4643dc30fec1c695e0e6545a2ce98
Mateusz Balasz <mateuszbalasz@Mateuszs-iMac-Pro.local>
2021-03-07 10:26:24 -0800

    Edit AppDelegate to connect to flutter code

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -2,12 +2,48 @@ import UIKit
 import Flutter
 
 @UIApplicationMain
-@objc class AppDelegate: FlutterAppDelegate {
-  override func application(
-    _ application: UIApplication,
-    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
-  ) -> Bool {
-    GeneratedPluginRegistrant.register(with: self)
-    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-  }
+@objc class AppDelegate: FlutterAppDelegate, FlutterStreamHandler {
+    var serviceStatusEventSink: FlutterEventSink?
+    
+    override func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+    ) -> Bool {
+        let controller : FlutterViewController = window?.rootViewController as! FlutterViewController
+        
+        let serviceStatusChannel = FlutterEventChannel(name: "plugins.beam/step_counter_service_status_plugin", binaryMessenger: controller.binaryMessenger)
+        
+        serviceStatusChannel.setStreamHandler(self)
+        
+        let channel = FlutterMethodChannel(name:"plugins.beam/step_counter_plugin",
+                                           binaryMessenger: controller.binaryMessenger)
+        channel.setMethodCallHandler({
+            [weak self] (call: FlutterMethodCall, result: FlutterResult) -> Void in
+            // Note: this method is invoked on the UI thread.
+            switch call.method {
+            case "StepCounterService.initializeService":
+                result("initializing")
+            case "StepCounterService.isInitialized":
+                result(false)
+            case "StepCounterService.stopService":
+                if let sink = self?.serviceStatusEventSink {
+                    sink(false)
+                }
+                result("stopping")
+            default:
+                result(FlutterMethodNotImplemented);
+            }
+        })
+        
+        GeneratedPluginRegistrant.register(with: self)
+        return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+    }
+    func onListen(withArguments arguments: Any?, eventSink events: @escaping FlutterEventSink) -> FlutterError? {
+        serviceStatusEventSink = events
+        return nil
+    }
+    
+    func onCancel(withArguments arguments: Any?) -> FlutterError? {
+        return nil
+    }
 }
